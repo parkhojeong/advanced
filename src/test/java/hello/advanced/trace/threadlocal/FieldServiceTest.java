6a1279a842af14fce4897c9c8de755f7b7c44319
hojeong <parkhj062@gmail.com>
2026-01-19 22:00:23 +0900

    [15. 동시성 문제 - 예제 코드]

---
+++ src/test/java/hello/advanced/trace/threadlocal/FieldServiceTest.java
@@ -0,0 +1,35 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() throws InterruptedException {
+        log.info("start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("userA");
+        Thread threadB = new Thread(userB);
+        threadB.setName("userB");
+
+        threadA.start();
+        Thread.sleep(100);
+        threadB.start();
+
+        Thread.sleep(2000); // countdownlatch
+        log.info("end");
+    }
+}
